61ad73ec94edf6dcfaa226013945af5d3212c146
DZAladan <48226177+DZAladan@users.noreply.github.com>
2025-04-09 21:13:01 +0200

    Italian localization
    I've made an Italian localization for Malverk.

---
+++ localization/it.lua
@@ -0,0 +1,43 @@
+return {
+	["misc"] = {
+		["dictionary"] = {
+			["b_textures"] = 'Texture',
+			["b_deck_skins"] = 'Personalizza mazzo',
+			b_apply = 'APPLICA',
+			b_applied = 'APPLICATO',
+			b_remove = 'RIMUOVI',
+			b_change = 'Cambiamenti',
+			malverk_low = 'MINIMO',
+			malverk_high = 'MASSIMO',
+			-- ['b_Cards'] = 'Carte da gioco',
+			-- ['b_Joker'] = 'Jolly',
+			-- ['b_Tarot'] = 'Tarocchi',
+			-- ['b_Planet'] = 'Carte Pianeta',
+			-- ['b_Spectral'] = 'Cardte Spettrali',
+			-- ['b_Enhanced'] = 'Potenziamenti',
+			-- ['b_Blind'] = 'Bui',
+			-- ['b_Tag'] = 'Patti',
+			-- ['b_Back'] = 'Mazzi',
+			-- ['b_Seal'] = 'Sigilli',
+			-- ['b_Voucher'] = 'Buoni',
+			-- ['b_Booster'] = 'Buste di espansione',
+        },
+    },
+	descriptions = {
+		malverk = {
+			config = {
+				text = {
+					'Configurazione #1#'
+				}
+			}
+		},
+		texture_packs = {
+			default = {
+				name = 'Predefinito',
+				text = {
+					'Texture del gioco di base'
+				}
+			}
+		}
+	}
+}
