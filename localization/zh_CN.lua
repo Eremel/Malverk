7f312687e75bcfbe4dcbc5e3697a3e812ee44160
HONE13 <54649543+HONE13@users.noreply.github.com>
2025-04-13 19:27:20 +0800

    localization-zh_CN
    Adding an zh_CN localization for Malverk.

---
+++ localization/zh_CN.lua
@@ -0,0 +1,43 @@
+return {
+	["misc"] = {
+		["dictionary"] = {
+			["b_textures"] = '纹理',
+			["b_deck_skins"] = '定制牌组',
+			b_apply = '应用',
+			b_applied = '起效',
+			b_remove = '移除',
+			b_change = '改变',
+			malverk_low = '低',
+			malverk_high = '高',
+			-- ['b_Cards'] = '游戏牌',
+			-- ['b_Joker'] = '小丑',
+			-- ['b_Tarot'] = '塔罗牌',
+			-- ['b_Planet'] = '星球牌',
+			-- ['b_Spectral'] = '幻灵牌',
+			-- ['b_Enhanced'] = '增强卡牌',
+			-- ['b_Blind'] = '盲注',
+			-- ['b_Tag'] = '标签',
+			-- ['b_Back'] = '牌组',
+			-- ['b_Seal'] = '蜡封',
+			-- ['b_Voucher'] = '优惠券',
+			-- ['b_Booster'] = '补充包',
+        },
+    },
+	descriptions = {
+		malverk = {
+			config = {
+				text = {
+					'配置 #1#'
+				}
+			}
+		},
+		texture_packs = {
+			default = {
+				name = '默认',
+				text = {
+					'基础游戏纹理'
+				}
+			}
+		}
+	}
+}
